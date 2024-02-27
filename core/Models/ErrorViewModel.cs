1d91a9201f02ecc1ff555df6efaa22d3bc2d9876
Santiago Gamboa <desarrolloqdata@outlook.com>
2024-02-27 14:25:04 -0500

    first commit

---
+++ core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
